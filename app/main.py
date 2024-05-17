bb2f0c554a8891ce6a073893d4f7db7aa73c1329
marsade <adenijifara@gmail.com>
2024-05-17 22:02:00 +0300

    Migration

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''Starts a flask application for the app'''
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 import requests
 import uuid
 
@@ -21,52 +21,98 @@ def home():
 @app.route('/refresh')
 def refresh():
     '''Redirects to the tracker page to return user information'''
-    url = f'{api_url}?apiKey={api_key}'
-    return redirect(url_for('tracker'))
+    user_ip = session.get('extracted_user_ip')
+    # Redirect to tracker with query parameter if user's IP is available
+    if user_ip:
+        return redirect(url_for('tracker', ip=user_ip))
+    else:
+        # Redirect without query parameter if user's IP is not available
+        return redirect(url_for('tracker'))
 
-
-@app.route('/tracker', strict_slashes=False, methods=['GET', 'POST'])
+@app.route('/tracker', strict_slashes=False, methods=['GET' ])
 def tracker():
     '''Tracker page for application'''
     cache_id = str(uuid.uuid4())
-    if 'ip_address' in session:
-        ip_address = session['ip_address']
-    else:
-        ip_address = None
+    ip_address = session.get('extracted_user_ip')# Get IP from query parameter or session
 
-    if 'domain_name' in session:
-        domain_name = session['domain_name']
-    else:
-        domain_name = None
+    domain_name = session.get('extracted_domain')
+    user_ip = session.get('extracted_user_ip')
+    print('Ip Address:', ip_address)
+    print('Domain Name:', domain_name)
+    print('user_ip:', user_ip)
 
-    if request.method == 'POST':
-        ip_input = request.form.get('ip_input')
-        if ip_input:
-            if ip_input[0].isalpha():
-                domain_name = ip_input
-                session['domain_name'] = ip_input  # Store Domain name in session
-            else:
-                ip_address = ip_input
-                session['ip_address'] = ip_input  # Store IP address in session
-        else:
-            # Clear IP address and Domain name if input is empty
-            ip_address = None
-            domain_name = None
-            session.pop('ip_address', None)
-            session.pop('domain_name', None)
-    elif request.method == 'GET':
-        ip_address = None
-        domain_name = None
-
+    if not ip_address and not domain_name:
+        return render_template('tracker.html', data=None,cache_id=cache_id)
     try:
         if ip_address:
             base_url = f'{api_url}?apiKey={api_key}&ipAddress={ip_address}'
         elif domain_name:
             base_url = f'{api_url}?apiKey={api_key}&domain={domain_name}'
         else:
-            base_url = f'{api_url}?apiKey={api_key}'
+            base_url = f'{api_url}?apiKey={api_key}&ipAddress={user_ip}'
         result = requests.get(base_url)
         data =  result.json()
-        return render_template('tracker.html', data=data, request_method=request.method, cache_id=cache_id)    
+        if 'code' in data:
+            return render_template('tracker.html', error="Invalid IP address or domain name.", cache_id=cache_id)
+        return render_template('tracker.html', data=data, request_method=request.method, cache_id=cache_id) 
     except requests.RequestException as e:
         print(f"Error fetching data: {e}")
+
+
+@app.route('/proxy', methods=['GET'])
+def proxy():
+    url = request.args.get('url')
+    if url:
+        response = requests.get(url)
+        return response.json()
+    else:
+        return jsonify({'error': 'URL parameter is missing'}), 400
+
+@app.route('/extract_ip', methods=['POST'])
+def extract_ip():
+    ip_address = ''
+    domain_name = ''
+
+    if not request.is_json:
+        # Handle form data
+        form_input = request.form.get('ip_input')
+        if form_input:
+            ip_input = form_input
+            if ip_input[0].isalpha():
+                domain_name = ip_input
+            else:
+                ip_address = ip_input
+        else:
+            ip_address = None
+            domain_name = None
+    else:
+        # Handle JSON request
+        ip_input = request.json.get('ip_address')
+        ip_address = ip_input
+        session['extracted_user_ip'] = ip_address
+    
+    session['extracted_ip'] = ip_address
+    session['extracted_domain'] = domain_name
+    
+
+    # Log the received IP address
+    print('Received IP address:', ip_address)
+    print('Received Domain name:', domain_name)
+    ext = session.get('extracted_user_ip')
+    if ext:
+        print('Received User Ip:', session['extracted_user_ip'])
+    return redirect(url_for('tracker'))
+
+@app.route('/get_extracted_ip', methods=['GET'])
+def get_extracted_ip():
+    # Assuming you have stored the extracted IP address in a session variable
+    extracted_ip = session.get('extracted_ip')
+    extracted_domain = session.get('extracted_domain')
+    user_ip = session.get('extracted_user_ip')
+
+    if extracted_ip:
+        return jsonify({'ip_address': extracted_ip, 'domain_name': None, 'user_ip': user_ip})
+    elif extracted_domain:
+        return jsonify({'ip_address': None, 'domain_name': extracted_domain, 'user_ip': user_ip})
+    else:
+        return jsonify({'ip_address': None})
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,3 +1,70 @@
+// Latitude and longitude for leaflet
+let reload = true
+if (reload) {
+  window.location = window.location
+  reload = false
+}
+
+function fetchIPAddress() {
+  // Fetch the extracted IP address from the backend
+  fetch('/get_extracted_ip')
+    .then(response => response.json())
+    .then(data => {
+      if (data.ip_address || data.domain_name) {
+        // Use the extracted IP address directly
+        console.log('Extracted IP address:', data.ip_address);
+        console.log('Extracted domain name:', data.domain_name);
+        // Exit the function here since we already have the IP address
+        return;
+      }
+      else {
+        FetchAndSendIpAddress();
+      }})
+    };
+
+
+function FetchAndSendIpAddress(){
+  fetch('/proxy?url=https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            const ipAddress = data.ip;
+            console.log('Fetched IP address:', ipAddress);
+            // Call a function to send the fetched IP address to the Flask backend
+            sendIPAddressToBackend(ipAddress);
+            // Proceed with other operations using the fetched IP address
+        })
+        .catch(error => {
+            console.error('Error fetching IP address:', error);
+        });
+}
+// Call the function to fetch the IP address 
+// Function to send the IP address to the Flask backend
+function sendIPAddressToBackend(ipAddress) {
+  const requestData = { ip_address: ipAddress };
+  console.log('Sending JSON data to backend:', requestData);
+  fetch('/extract_ip', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestData) // Send the IP address in the request body
+  })
+  .then(response => {
+      if (response.ok) {
+
+          console.log('IP address sent to backend successfully');
+      } else {
+          console.error('Failed to send IP address to backend');
+      }
+  })
+  .catch(error => {
+      console.error('Error sending IP address to backend:', error);
+  });
+}
+
+// Call the function to fetch the IP address when the page loads
+window.onload = fetchIPAddress;
+
 let lat = document.querySelector('.lat').innerHTML;
 let long = document.querySelector('.long').innerHTML;
 
